3e8a5756ce05978d6a7708a840b4394ab5286c0e
Gabriel <biel_borel@hotmail.com>
2022-08-11 16:12:28 -0300

    Added express, typescript and ts-node-dev to the application

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const $PORT = 3333;
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello World" });
+});
+
+app.listen($PORT, () => console.log(`Server is running on port ${$PORT}.`));
