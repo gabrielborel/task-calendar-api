12a7684f693a7b175c1bbd66ba5013b6835aafeb
Gabriel <biel_borel@hotmail.com>
2022-08-15 15:38:58 -0300

    Some Fixes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import database from "./config/dbConnect";
+import router from "./routes";
 
 const $PORT = 3333;
-const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+database.on("error", console.log.bind(console, "Erro de conexão"));
+database.once("open", () => {
+  console.log("Connected with database.");
 });
+
+const app = express();
+router(app);
 
 app.listen($PORT, () => console.log(`Server is running on port ${$PORT}.`));
