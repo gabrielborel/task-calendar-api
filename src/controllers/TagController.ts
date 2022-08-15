79a101daa8472d5931990dcb184e68b695cd41a1
Gabriel <biel_borel@hotmail.com>
2022-08-15 16:35:30 -0300

    Added create/remove/update methods

--- src/controllers/TagController.ts
+++ src/controllers/TagController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { CallbackError } from "mongoose";
 import Tag from "../models/Tag";
 
 export default class TagController {
@@ -13,5 +14,30 @@ export default class TagController {
     });
 
     res.status(201).send({ newTag });
+  }
+
+  static remove(req: Request, res: Response) {
+    const { id } = req.params;
+
+    Tag.findByIdAndDelete(id, (err: CallbackError) => {
+      if (err) {
+        res.status(500).send({ message: err.message });
+      }
+    });
+
+    res.status(200).send({ message: "Tag removed successfully" });
+  }
+
+  static async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, color } = req.body;
+
+    Tag.findByIdAndUpdate(id, { $set: { name, color } }, (err) => {
+      if (err) {
+        res.status(500).send({ err });
+      }
+    });
+
+    res.status(200).send({ message: "Tag updated successfully" });
   }
 }
